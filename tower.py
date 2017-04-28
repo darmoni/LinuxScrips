36aa4fd175817c2acf7b1a2df9f5c08ea49fc7f8
Nir Darmoni <NDarmoni@xcastlabs.com>
2017-04-28 18:42:02 -0500

    hanoy towers
        new file:   anoii.py
        new file:   tower.py

---
+++ tower.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+from anoii import towers
+
+def main(argv):
+
+    number = "Number = "
+    word ="המספר =".decode('UTF-8')
+    rtl_number =''.join(reversed(word))
+
+    if(len(argv)):
+        _number = int(argv[0])
+    else:
+        _number = 1
+
+    rtl = (_number%2)
+    tower = towers(rtl)
+    print "\n"
+    if(rtl):
+        print "\t\t\t",_number, rtl_number
+    else:
+        print number,_number
+    tower.towers(_number,0,1,2)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+
