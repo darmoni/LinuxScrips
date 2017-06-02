51ae702fc8728c6a4ef8b4cb74a8f82a4ab17f7c
Nir Darmoni <NDarmoni@xcastlabs.com>
2017-06-02 17:44:14 -0500

    modified:   multi_log_2.influx.py 	new file:   staging_conf_2_influxdb.sh 	new file:   staging_conf_only.sh 	modified:   test_case_matrix.csv

--- multi_log_2.influx.py
+++ multi_log_2.influx.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
-import shlex, subprocess, socket, os, time, signal, sys
+import shlex, subprocess, socket, os, time, signal, sys, datetime
 from time import sleep
 from subprocess import call, Popen, check_output, PIPE
 from nbstreamreader import NonBlockingStreamReader as NBSR
 
 class log_collector:
-    def __init__(self, log ='db', log_server = 'xdev64.xcastlabs.com'):
+    def __init__(self, log ='db', log_server = 'xdev64.xcastlabs.com', syslog=False):
         self._server=log_server
         self._log=log
         self._p = -1
-        self._params =self.add_dev_log(self._log,self._server)
+        if syslog:self._params =self.add_syslog_log(self._log,self._server)
+        else :self._params =self.add_local_log(self._log,self._server)
 
     def collect_log(self):
         (remote_log_srv_cmd,log_srv_cmd,parser,influx_writer) = self._params
@@ -19,7 +20,7 @@ class log_collector:
             self._p.stdin.write(log_srv_cmd)
             return self._p
 
-    def add_dev_log(self, log, log_server):
+    def add_local_log(self, log, log_server):
 
         influx_writer='/home/nir/bin/table_2_db.py'
         logname = "~/logs/{}.log".format(log)
@@ -30,6 +31,22 @@ class log_collector:
             parser = '/home/nir/bin/qman_2_table.awk'
         elif('conf' == log):
             logname = "~/logs/{}.log".format('hstarter')
+
+        remote_log_srv_cmd ="ssh xcast@{}".format(log_server)
+        log_srv_cmd ="tail -0f {}\n".format(logname)
+        return remote_log_srv_cmd,log_srv_cmd,parser,influx_writer
+
+    def add_syslog_log(self, log, log_source, log_server = "logserver3-la.siptalk.com"):
+
+        influx_writer='/home/nir/bin/table_2_db.py'
+        logname = "~/logs/servers/{}/{}/{}.log".format(log_source, datetime.datetime.today().strftime("%Y%m%d"),log)
+        parser = 'awk -v name={} -f /home/nir/bin/log_2_table.awk'.format(log)
+        if('db' == log):
+             parser = '/home/nir/bin/db_2_table.awk'
+        elif('qman' == log):
+            parser = '/home/nir/bin/qman_2_table.awk'
+        elif('conf' == log):
+            logname = "~/logs/servers/{}/{}/{}.log".format(log_source, datetime.datetime.today().strftime("%Y%m%d"),'hstarter')
 
         remote_log_srv_cmd ="ssh xcast@{}".format(log_server)
         log_srv_cmd ="tail -0f {}\n".format(logname)
@@ -57,7 +74,7 @@ class log_collector:
             p.kill()
             return
 
-    def run(self, execute_cmd='./dev_conf_2_influxdb.sh'):
+    def run(self, execute_cmd='~/bin/dev_conf_2_influxdb.sh'):
         (remote_log_srv_cmd,log_srv_cmd,parser,influx_writer) = self._params
         self.setup()
         try:
@@ -84,33 +101,42 @@ class log_collector:
 #production_qman_log_collector=log_collector('qman','tswitch3.siptalk.com')
 #production_qman_log_collector.run('sleep 50')
 
-dev_db_log_collector=log_collector('db')
-dev_db_log_collector.collect_log()
+db_log_collector=log_collector('db',"stage1n1-la.siptalk.com",True)
+#dev_db_log_collector=log_collector('db')
+db_log_collector.collect_log()
 
-dev_mserver_log_collector=log_collector('mserver')
-dev_mserver_log_collector.collect_log()
+#dev_mserver_log_collector=log_collector('mserver',"mserver1n1-la.siptalk.com",True)
+mserver_log_collector=log_collector('mserver',"mserver1n1-la.siptalk.com")
+mserver_log_collector.collect_log()
+conf_log_collector=log_collector('conf',"mserver1n1-la.siptalk.com",True)
+conf_log_collector.run('./staging_conf_only.sh')
+#check_output('~/bin/staging_conf_only.sh')
 
-dev_qman_log_collector=log_collector('qman')
-dev_qman_log_collector.run('test.py -p qman_dev')#('time sleep {}'.format(60*60))#('time sleeper.sh 120')('./dev_qman_2_influxdb.sh')
+#dev_qman_log_collector=log_collector('qman')
+#dev_qman_log_collector.run('test.py -p qman_dev')#('time sleep {}'.format(60*60))#('time sleeper.sh 120')('./dev_qman_2_influxdb.sh')
 
-dev_mserver_log_collector._p.kill()
-dev_db_log_collector._p.kill()
+sleep(30)
 
-#(remote_log_srv_cmd,log_srv_cmd,parser,influx_writer) = add_dev_log('mserver')
-#(remote_log_srv_cmd,log_srv_cmd,parser,influx_writer) = add_dev_log('db')
+mserver_log_collector._p.kill()
+db_log_collector._p.kill()
+
+sleep(30)
+
+#(remote_log_srv_cmd,log_srv_cmd,parser,influx_writer) = add_local_log('mserver')
+#(remote_log_srv_cmd,log_srv_cmd,parser,influx_writer) = add_local_log('db')
 #run (remote_log_srv_cmd,log_srv_cmd,parser,influx_writer,'./dev_conf_2_influxdb.sh')
-#(remote_log_srv_cmd,log_srv_cmd,parser,influx_writer) = add_dev_log('mserver')
+#(remote_log_srv_cmd,log_srv_cmd,parser,influx_writer) = add_local_log('mserver')
 #print "{} '{}' | {} | {}".format(remote_log_srv_cmd,log_srv_cmd,parser,influx_writer)
 #run (remote_log_srv_cmd,log_srv_cmd,parser,influx_writer,'./dev_qman_2_influxdb.sh')
 
 '''
-print "{} '{}' | {} | {}".format(*add_dev_log())
-print "{} '{}' | {} | {}".format(*add_dev_log('mserver'))
-print "{} '{}' | {} | {}".format(*add_dev_log('conf'))
-print "{} '{}' | {} | {}".format(*add_dev_log('db'))
-print "{} '{}' | {} | {}".format(*add_dev_log('qman'))
+print "{} '{}' | {} | {}".format(*add_local_log())
+print "{} '{}' | {} | {}".format(*add_local_log('mserver'))
+print "{} '{}' | {} | {}".format(*add_local_log('conf'))
+print "{} '{}' | {} | {}".format(*add_local_log('db'))
+print "{} '{}' | {} | {}".format(*add_local_log('qman'))
 print './dev_conf_2_influxdb.sh'
-print "{} '{}' | {} | {}".format(*add_dev_log('qman'))
+print "{} '{}' | {} | {}".format(*add_local_log('qman'))
 print './dev_qman_2_influxdb.sh'
 '''
 sys.exit(0)
