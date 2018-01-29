07ec3964a3ba47028c7642e3c9e0596e77efda2b
Nir Darmoni <NDarmoni@xcastlabs.com>
2018-01-29 09:45:54 -0600

    float time, drop & create table

--- reg_event_listener.py
+++ reg_event_listener.py
@@ -1,16 +1,89 @@
 #!/usr/bin/env python
 
 """
-insert into active_table event='ACTIVE', time=1516925339374555136, calls='0', dialogs='0', extra='R:21', full='0', regs='21', serverip='10.10.10.62', started='1516742939'
-insert into bye_table event='BYE', time=1516925264825032960, direction='CLN', domain='siptalk64.xcastlabs.com', serverip='10.10.10.62', uniqno='72806'
-insert into cancel_table event='CANCEL', time=1516976718519753984, direction='SRV', domain='siptalk64.xcastlabs.com', serverip='10.10.10.62', uniqno='74545'
-insert into dtor_table event='DTOR', time=1516925304825748992, domain='siptalk64.xcastlabs.com', serverip='10.10.10.62', uniqno='72806'
-insert into faild_table event='FAILD', time=1516925728514002944, agent='Cisco/SPA303-7.6.2c', callid='6a0fecd4-6f19d202@10.0.0.13', domain='siptalk64.xcastlabs.com', extip='67.167.37.164', intip='10.0.0.13', line='SPA303-0001-01', reason='Not our Device', serverip='10.10.10.62'
-insert into invite_table event='INVITE', time=1516925232999401216, direction='CLN', domain='siptalk64.xcastlabs.com', from='GS0001-0001-02', serverip='10.10.10.62', to='9801', uniqno='72806'
-insert into reg_table event='REG', time=1516925236676583168, aor='sip:TestVVXPOL401L02*siptalk64.xcastlabs.com-75.145.154.225+16053@75.145.154.234:7064', agent='PolycomVVX-VVX_401-UA/5.7.0.11768', callid='03ba66a09b5f474da3abd51c91016a63', domain='siptalk64.xcastlabs.com', extip='75.145.154.225', intip='10.10.8.46', line='TestVVXPOL401L02', serverip='10.10.10.62'
-insert into talk_table event='TALK', time=1516925233086228992, domain='siptalk64.xcastlabs.com', fmode='-', serverip='10.10.10.62', uniqno='72806'
-insert into unreg_table event='UNREG', time=1516925303046435072, aor='sip:GS0001-0001-02-0x1706800*siptalk64.xcastlabs.com-75.145.154.225+45338@75.145.154.234:7064', domain='siptalk64.xcastlabs.com', line='GS0001-0001-02-0x1706800', serverip='10.10.10.62'
+DROP TABLE IF EXISTS invite_table;
+CREATE TABLE  invite_table
+(
+recdate Date MATERIALIZED toDate(time)
+, direction String, domain String, event String, from String, serverip String, time Float64, to String, uniqno String
+)
+ENGINE = MergeTree(recdate, (recdate,serverip), 8192);
+#PARTITION BY recdate ORDER BY recdate;
+
+insert into invite_table (domain,from,direction,serverip,uniqno,to,time,event) values('siptalk64.xcastlabs.com','GS0001-0001-02','CLN','10.10.10.62','84330','9801',1517236893.594013929,'INVITE');
+insert into invite_table (domain,from,direction,serverip,uniqno,to,time,event) values('siptalk64.xcastlabs.com','GS0001-0001-02','CLN','10.10.10.62','84330','9801',1517236893.633804083,'INVITE');
+DROP TABLE IF EXISTS talk_table;
+CREATE TABLE  talk_table
+(
+recdate Date MATERIALIZED toDate(time)
+, domain String, event String, fmode String, serverip String, time Float64, uniqno String
+)
+ENGINE = MergeTree(recdate, (recdate,serverip), 8192);
+#PARTITION BY recdate ORDER BY recdate;
+
+insert into talk_table (domain,serverip,uniqno,fmode,time,event) values('siptalk64.xcastlabs.com','10.10.10.62','84330','-',1517236894.690232038,'TALK');
+DROP TABLE IF EXISTS bye_table;
+CREATE TABLE  bye_table
+(
+recdate Date MATERIALIZED toDate(time)
+, direction String, domain String, event String, serverip String, time Float64, uniqno String
+)
+ENGINE = MergeTree(recdate, (recdate,serverip), 8192);
+#PARTITION BY recdate ORDER BY recdate;
+
+insert into bye_table (domain,direction,serverip,uniqno,time,event) values('siptalk64.xcastlabs.com','SRV','10.10.10.62','84330',1517236894.897785902,'BYE');
+DROP TABLE IF EXISTS reg_table;
+CREATE TABLE  reg_table
+(
+recdate Date MATERIALIZED toDate(time)
+, aor String, agent String, callid String, domain String, event String, extip String, intip String, line String, serverip String, time Float64
+)
+ENGINE = MergeTree(recdate, (recdate,serverip), 8192);
+#PARTITION BY recdate ORDER BY recdate;
+
+insert into reg_table (domain,callid,intip,agent,serverip,aor,time,line,extip,event) values('siptalk64.xcastlabs.com','efd00b116ba451d@10.10.8.10','10.10.8.10','Htek UC840 V1.0.4.2.11','10.10.10.62','sip:UC8xx-0001-01*siptalk64.xcastlabs.com-75.145.154.225+5260@75.145.154.234:7064',1517236907.523650885,'UC8xx-0001-01','75.145.154.225','REG');
+insert into reg_table (domain,callid,intip,agent,serverip,aor,time,line,extip,event) values('siptalk64.xcastlabs.com','7446951c3234ab298082901156869702','10.10.8.210','Polycom/5.5.0.20556 PolycomVVX-VVX_410-UA/5.5.0.20556','10.10.10.62','sip:POL500-0003-01*siptalk64.xcastlabs.com-75.145.154.225+41742@75.145.154.234:7064',1517236925.390005112,'POL500-0003-01','75.145.154.225','REG');
+DROP TABLE IF EXISTS dtor_table;
+CREATE TABLE  dtor_table
+(
+recdate Date MATERIALIZED toDate(time)
+, domain String, event String, serverip String, time Float64, uniqno String
+)
+ENGINE = MergeTree(recdate, (recdate,serverip), 8192);
+#PARTITION BY recdate ORDER BY recdate;
+
+insert into dtor_table (uniqno,domain,event,serverip,time) values('84330','siptalk64.xcastlabs.com','DTOR','10.10.10.62',1517236934.897223949);
+DROP TABLE IF EXISTS faild_table;
+CREATE TABLE  faild_table
+(
+recdate Date MATERIALIZED toDate(time)
+, agent String, callid String, domain String, event String, extip String, intip String, line String, reason String, serverip String, time Float64
+)
+ENGINE = MergeTree(recdate, (recdate,serverip), 8192);
+#PARTITION BY recdate ORDER BY recdate;
+
+insert into faild_table (domain,callid,intip,agent,reason,serverip,time,line,extip,event) values('siptalk64.xcastlabs.com','4c2ad322-23efe7fa@10.0.0.13','10.0.0.13','Cisco/SPA303-7.6.2c','Not our Device','10.10.10.62',1517236985.137672901,'SPA303-0001-01','67.167.37.164','FAILD');
+
+DROP TABLE IF EXISTS active_table;
+CREATE TABLE  active_table
+(
+recdate Date MATERIALIZED toDate(time)
+, calls String, dialogs String, event String, extra String, full String, regs String, serverip String, started String, time Float64
+)
+ENGINE = MergeTree(recdate, (recdate,serverip), 8192);
+#PARTITION BY recdate ORDER BY recdate;
+
+insert into active_table (full,dialogs,calls,extra,started,regs,serverip,time,event) values('0','0','0','R:22','1516742939','22','10.10.10.62',1517237039.363743067,'ACTIVE');
+insert into reg_table (domain,callid,intip,agent,serverip,aor,time,line,extip,event) values('russ.xcastlabs.com','5326ef0a-c226ff31@10.0.0.207','10.0.0.207','Linksys/SPA1001-3.1.19(SE)','10.10.10.62','sip:312D01L01*russ.xcastlabs.com-67.167.37.164+16195@75.145.154.234:7064',1517237115.241877079,'312D01L01','67.167.37.164','REG');
+insert into reg_table (domain,callid,intip,agent,serverip,aor,time,line,extip,event) values('siptalk64.xcastlabs.com','1471544780@192.168.1.173','192.168.1.173','Yealink SIP-T22P 7.73.193.50','10.10.10.62','sip:7131YL01*siptalk64.xcastlabs.com-173.15.117.197+35932@75.145.154.234:7064',1517237244.529793024,'7131YL01','173.15.117.197','REG');
+insert into invite_table (domain,from,direction,serverip,uniqno,to,time,event) values('siptalk64.xcastlabs.com','GS0001-0001-02','CLN','10.10.10.62','84345','9801',1517237287.197594881,'INVITE');
+insert into invite_table (domain,from,direction,serverip,uniqno,to,time,event) values('siptalk64.xcastlabs.com','GS0001-0001-02','CLN','10.10.10.62','84345','9801',1517237287.235697985,'INVITE');
+insert into talk_table (domain,serverip,uniqno,fmode,time,event) values('siptalk64.xcastlabs.com','10.10.10.62','84345','-',1517237287.302793980,'TALK');
+insert into bye_table (domain,direction,serverip,uniqno,time,event) values('siptalk64.xcastlabs.com','SRV','10.10.10.62','84345',1517237321.107734919,'BYE');
+insert into active_table (full,dialogs,calls,extra,started,regs,serverip,time,event) values('0','2','0','R:22,-:1','1516742939','22','10.10.10.62',1517237339.364963055,'ACTIVE');
+insert into dtor_table (uniqno,domain,event,serverip,time) values('84345','siptalk64.xcastlabs.com','DTOR','10.10.10.62',1517237361.107492924);
 """
+
 from socket import socket, AF_INET, SOCK_DGRAM, gethostbyname
 MAX_SIZE=4096
 import time
@@ -18,32 +91,37 @@ from Queue import Queue
 from threading import Thread
 
 xcast_event_tables={}
-non_string_fields={'Time':'UInt64', 'Unix_TS':'UInt64'}
+non_string_fields={'Time':'Float64'}
+
 
 class xcast_event_table:
 
     def print_tables(self):
         print(xcast_event_tables)
 
-    def __init__(self,record,ev_type,table_name):
+    def __init__(self,record,ev_type,table_name,even_if_exists=False):
         if( table_name in xcast_event_tables):
             pass
         else:
             other_fields=''
+            create_even_if_exists= ("IF NOT EXISTS ","") [even_if_exists]
             for key in sorted(record.keys()):
                 if (key in non_string_fields): 
                     type_name=non_string_fields[key]
                 else:
                     type_name='String'
                 other_fields += (", {} {}".format(key.lower(), type_name))
-            create_new_table="""CREATE TABLE {}
+            drop_old_table="DROP TABLE IF EXISTS {};".format(table_name)
+            if(even_if_exists):
+                print(drop_old_table)
+            create_new_table="""CREATE TABLE {} {}
 (
-recdate Date MATERIALIZED toDate(unix_ts)
+recdate Date MATERIALIZED toDate(time)
 {}
 )
 ENGINE = MergeTree(recdate, (recdate,serverip), 8192);
 #PARTITION BY recdate ORDER BY recdate;
-""".format(table_name, other_fields)
+""".format(create_even_if_exists, table_name, other_fields)
             print(create_new_table)
             xcast_event_tables.update({table_name:create_new_table})
     def __del__ (self):
@@ -90,7 +168,7 @@ def prepare_insert_query(q):
             #for key in sorted(record.keys()):
             #    value = record[key]
             #    query += (", {}='{}'".format(key.lower(),value))
-            #print(query)
+            print(query)
         q.task_done()
 
 if __name__ == '__main__':
@@ -107,6 +185,6 @@ if __name__ == '__main__':
         #print(msg)
         if(data):
             when=time.time()
-            data += "\nUnix_TS: {} \nTime: {} \nServerIP: {}".format(int(when),int(when * 1000000000), addr[0])
+            data += "\nTime: {:18.9f} \nServerIP: {}".format(when, addr[0])
             to_db(data,events_q)
 
