2c9e55eba71f753107e2c2ded31d913e4ca8dc2a
Nir Darmoni <NDarmoni@xcastlabs.com>
2019-05-31 08:48:45 -0500

    Systemd Unita & wakeup

---
+++ start_rpmbuilds.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python;
+export WORKON_HOME=$HOME/Envs;
+export PROJECT_HOME=$HOME/VirtualProjects;
+export VIRTUALENVWRAPPER_LOG_FILE=hook.log;
+export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh;
+source $VIRTUALENVWRAPPER_SCRIPT;
+
+workon rpmbuilds && python manage.py runserver
