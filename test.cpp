4d32c0a49f09dd8a904579478c34c7975503d299
Nir Darmoni <NDarmoni@xcastlabs.com>
2019-09-20 11:01:32 -0500

    Back to sshfs. gio mounts w/o write access
    	modified:   test.cpp
    	modified:   wakeup

--- test.cpp
+++ test.cpp
@@ -6,6 +6,29 @@
 #include <sstream>
 using namespace std;
 
+
+/*
+ * C++ Reserved or Non-graphic Characters
+ *
+ *
+ | C haracter | ASCII Representation | ASCII Value | Escape Sequence |
+ | ---------- | --------------------- | ----------- | --------------- |
+ | Newline | NL (LF) | 10 | \n |
+ | Horizontal tab | HT | 9 | \t |
+ | Vertical tab | VT | 11 | \v |
+ | Backspace | BS | 8 | \b |
+ | Carriage return | CR | 13 | \r |
+ | Formfeed | FF | 12 | \f |
+ | Alert | BEL | 7 | \a |
+ | Backslash | \ | 92 | \\ |
+ | Question mark | ? | 63 | \? |
+ | Single quotation mark | ' | 39 | \' |
+ | Double quotation mark | " | 34 | \" |
+ | Octal number | ooo | -- | \ooo |
+ | Hexadecimal number | hhh | -- | \xhhh |
+ | Null character | NUL | 0 | \0 |
+
+*/
 std::string& ltrim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
 {
     str.erase(0, str.find_first_not_of(chars));
@@ -25,7 +48,7 @@ std::string& trim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
 
 int main(void)
 {
-    string s(" \t \n This string has leading, and trailing white-space chars   \n");
+    string s(" \t \n This string has leading, \r\fand\vtrailing white-space charsending here\n");
     string replacing(" has ");
     cout << "Before trim \n'" << s << "'\n";
     trim(s);
