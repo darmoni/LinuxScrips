4c552e06af00676ad3135d4e0be893fb40c27e2a
Nir Darmoni <NDarmoni@xcastlabs.com>
2020-03-23 07:06:18 -0500

    Adding RepoStatus, RepoPrepare
     Date:      Mon Mar 23 07:06:18 2020 -0500
    
    	modified:   get_project_commands.sh

--- get_project_commands.sh
+++ get_project_commands.sh
@@ -10,12 +10,26 @@ if [[ "Prepare" =~ "$*" ]] ; then
         # less verbose than 'make clean'
         ssh ndarmoni@${build_server} "cd git_rpm_scripts ; make clean_clean"
     done
+elif [[ "RepoPrepare" =~ "$*" ]] ; then
+    for build_server in ${servers} ; do
+        echo "
+        Repo Preparing ${build_server}
+        "
+        ssh ndarmoni@${build_server} "cd google_repo_rpm/.repo/manifests && make clean && make branches"
+    done
 elif [[ "Status" =~ "$*" ]] ; then
     for build_server in ${servers} ; do
         echo "
         Statusing ${build_server}
         "
         ssh ndarmoni@${build_server} "cd git_rpm_scripts/Registrator ; git sst"
+    done
+elif [[ "RepoStatus" =~ "$*" ]] ; then
+    for build_server in ${servers} ; do
+        echo "
+        Repo Statusing ${build_server}
+        "
+        ssh ndarmoni@${build_server} "cd google_repo_rpm/.repo/manifests && repo status"
     done
 else
     ssh ndarmoni@pbxdev.xcastlabs.com "cd git_rpm_scripts ; ./change_manager.py $*"
