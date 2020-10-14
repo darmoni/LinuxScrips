b2f0e8bfed3d6ba351d57a6fc74b4f8ae58f45d9
Nir Darmoni <NDarmoni@xcastlabs.com>
2020-10-14 08:45:36 -0500

    From local branches
    	new file:   test_hardlink.cpp
    	new file:   try.go
    	new file:   tunnel_it.sh

--- tunnel_it.sh
+++ tunnel_it.sh
@@ -36,3 +36,10 @@ fi
 
  
  
+=======
+# Using sudo
+if [ "" == "$(pgrep -f vpnc-connect)" ]; then sudo vpnc-connect default
+else echo 'VPN is Already on'
+[[ $(ps -ef | grep 'ssh -D ' | grep -v $$ ) ]] && echo 'Tunnel is already on' || ssh -D 1080  xcast@10.10.10.31
+fi
+ 
