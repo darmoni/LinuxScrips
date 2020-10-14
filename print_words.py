3e9002fc063c5b7df3125ea55b4de24c921f6e3e
Nir Darmoni <NDarmoni@xcastlabs.com>
2020-10-14 11:08:56 -0500

    Cleanup

--- print_words.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-words={}
-for word in ('db','ghost','db','ACD'):
-    if word in words:
-        words[word] +=1
-    else:
-        words[word]=1
-print(words)
--- udp_trigger.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from socket import socket, AF_INET, SOCK_DGRAM
-
-PORT = 12345
-sock = socket(AF_INET,SOCK_DGRAM)
-msg = "Hello UDP server"
-for counter in range(3):
-    sock.sendto(msg.encode('utf-8'),('38.102.250.167', PORT))
-   
