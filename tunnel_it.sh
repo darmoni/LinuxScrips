66dd6ed6b281910858950beee27b93cc8714a8b8
Nir Darmoni <NDarmoni@xcastlabs.com>
2020-10-19 16:17:29 -0500

    TRYTUNNEL
    	modified:   tunnel_it.sh

--- tunnel_it.sh
+++ tunnel_it.sh
@@ -18,15 +18,15 @@
 
 
 # Using sudo
-TRYUNNEL=0
+TRYTUNNEL=0
 if [ ! $(pgrep -f vpnc-connect) ]; then sudo vpnc-connect default; sudo ufw enable
 ssh nir@ubuntu-darmoni.xcastlabs.com 'nohup  ~/bin/start_rpmbuilds.sh &' &
 else echo 'VPN is Already on'; \
-TRYUNNEL=1
+TRYTUNNEL=1
 ssh nir@ubuntu-darmoni.xcastlabs.com 'ls -l > /dev/null'
 fi
 
-if [ "1" != "$TRYUNNEL" ] ; then echo "done";
+if [ "1" != "$TRYTUNNEL" ] ; then echo "done";
 else if [ $(pgrep -f 'ssh -D ') ]
 	then echo 'Tunnel is already on';
 	#else ssh -D 1080  nir@10.10.10.55;
