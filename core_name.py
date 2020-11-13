f02e18488dc78c6c4e2e8fa4efb0eb32ea10de40
Nir Darmoni <NDarmoni@xcastlabs.com>
2020-11-13 07:49:21 -0600

    Working with core_name.py
    	new file:   checked_cores_list.doc
    	new file:   core_name.py
    	modified:   monitor_core_pbxsm-production.sh

---
+++ core_name.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+
+from subprocess import check_output
+import shlex
+from sys import stdin
+
+core_modules = {}
+hashes = {}
+new_cores = []
+processed = []
+filter = 'pbxsm-production'
+
+def read_processed(file):
+    try:
+        with open(file) as f:
+            for line in f.readlines():
+                processed.append(line.strip())
+                #print("found in processed file ", processed)
+    except Exception:
+        print("problem with file {}".format(file))
+        exit()
+
+def core_parts(new_core, platform):
+    pos = new_core.find(platform)
+
+def read_cores(lines):
+    for core in lines:
+        if core.strip().find(filter) >= 0:
+            if core.strip() not in processed:
+                new_cores.append(core.strip())
+
+def suggest_logs(hashes):
+    existing_hashes = []
+    cores = hashes.keys()
+    for hash in cores:
+        core_module = hashes[hash][0]
+        #print("hash = '{}', module = '{}'".format(hash, core_module))
+
+        flog_cmd = "ssh ndarmoni@logs-pbx-la.xcastlabs.net 'flog {}'".format(core_module)
+        log_list = check_output(shlex.split(flog_cmd)).decode().splitlines()
+        for log_name in log_list:
+            if not ((log_name.find('ls -latr') > -1) or (log_name.find('errors.log') > -1)):
+                for core_hash in cores:
+                    if log_name.find(hash) > -1:
+                        print(log_name)
+
+read_processed('checked_cores_list.doc')
+#print("found in processed list ", processed)
+
+temp = stdin.read().splitlines()
+read_cores (temp)
+
+for p in processed:
+    print("Processed core: '{}'".format(p))
+#print("found in new cores list ", new_cores)
+
+for line in new_cores:
+    print("New core: '{}'".format(line))
+    line_parts = line.split("/")
+    if len(line_parts) < 5:
+        next
+    name_parts = line_parts[4].split(".")
+    for i in name_parts:
+        if i.find(filter) >= 0:
+            name = i
+            parts = name.split('-')
+            #print("parts are '{}'".format(parts))
+            if 4 == len(parts):
+                hash = parts[3]
+                core_module = '-'.join( parts[0:4])
+                #print("hash = '{}', core_module = '{}'".format(hash,core_module ))
+                hashes.update({hash:[core_module]})
+
+suggest_logs(hashes)
--- monitor_core_pbxsm-production.sh
+++ monitor_core_pbxsm-production.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
-check_cores.sh 2> /dev/null | grep core | sort -u | awk -f ~/bin/core_name.awk | sort -u | grep -v errors.log | egrep 'container[[:digit:]]+-la.xcastlabs.net|[[:alpha:]]+-pbxsm-production-'
+check_cores.sh 2> /dev/null | sort -u | grep -v container | core_name.py | sort -u | grep -v errors.log
+#check_cores.sh 2> /dev/null | grep core | sort -u | ~/bin/core_name.py | sort -u | grep -v errors.log | egrep 'container[[:digit:]]+-la.xcastlabs.net|[[:alpha:]]+-pbxsm-production-'
